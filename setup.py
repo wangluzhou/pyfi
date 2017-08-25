e67cb615980cd3facb48e652799b86315c1342bf
wangluzhou <wangluzhou@aliyun.com>
2017-08-25 13:18:43 +0800

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+# encoding: utf-8
+from setuptools import setup, find_packages
+import sys, os
+
+VERSION = '0.1.7'
 
 setup(name='windHelper',
-      version='0.1',
+      version=VERSION,
       description='encapsulate the windPy API with pandas',
       url='https://github.com/wangluzhou/windHelper.git',
       author='wangluzhou',
       author_email='wangluzhou@aliyun.com',
       license='MIT',
       packages=['windHelper'],
-      zip_safe=False)
+      zip_safe=True)
